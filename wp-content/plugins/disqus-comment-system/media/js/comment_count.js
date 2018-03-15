0254e95d5a4d1dfb4198b987393e4db79a73c5c2
Kleber Lima da Silva <kleber.ufu@gmail.com>
2018-03-14 21:19:40 -0300

    Atualizações do tema e plugins

--- wp-content/plugins/disqus-comment-system/media/js/comment_count.js
+++ wp-content/plugins/disqus-comment-system/media/js/comment_count.js
@@ -10,8 +10,9 @@ var disqus_shortname = countVars.disqusShortname;
             nodes[i].parentNode.href = url + '#disqus_thread';
         }
     }
-    var s = document.createElement('script'); s.async = true;
+    var s = document.createElement('script');
+    s.async = true;
     s.type = 'text/javascript';
-    s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+    s.src = 'https://' + disqus_shortname + '.disqus.com/count.js';
     (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
 }());
